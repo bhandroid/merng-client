e52143c30204a13d7130dbff1ca10f56a68b6015
BHANU PRAKASH RAMINENI <77657678+bhandroid@users.noreply.github.com>
2021-04-17 12:43:08 +0530

    Update ApolloProvider.js

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "https://damp-hollows-27806.herokuapp.com/",
+  uri: "https://damp-hollows-27806.herokuapp.com",
 });
 
 const authLink = setContext(() => {
