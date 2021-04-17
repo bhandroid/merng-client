698e9487122cfafd940b9a0b301fd77513bbd0e7
bhandroid <bhanuprakash0863@gmail.com>
2021-04-17 12:33:00 +0530

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://damp-hollows-27806.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
